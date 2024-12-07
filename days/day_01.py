345ac78099f638a0ae8c7a96742ca2f5b0a3a26f
Clement Gaboriau Couanau <clement.gaboriaucouanau@datadoghq.com>
2024-12-07 22:57:40 +0100

    [DAY-01] Solution of the 2 stars of Day 1

--- days/day_01.py
+++ days/day_01.py
@@ -1,26 +1,35 @@
-from typing import List
+from typing import Iterator
 
 from day_factory.day import Day
 from day_factory.day_utils import TestEnum
 
 
 class Day01(Day):
-    FIRST_STAR_TEST_RESULT = 0
-    SECOND_STAR_TEST_RESULT = 0
+    FIRST_STAR_TEST_RESULT = 3
+    SECOND_STAR_TEST_RESULT = 1
 
     def __init__(self):
         super().__init__(self)
 
     @staticmethod
-    def count_id(input_value: List[str]) -> int:
-        return 0
+    def count_id(input_value: Iterator[str]) -> int:
+        return sum(map(lambda x: 1 if x == "(" else -1, next(input_value)))
 
     @staticmethod
-    def count_id_2(input_value: List[str]) -> int:
-        return 0
+    def count_id_2(input_value: Iterator[str]) -> int:
+        instructions = next(input_value)
+        floor = 0
+        for i, c in enumerate(instructions):
+            floor += 1 if c == "(" else -1
+            if floor == -1:
+                return i + 1
 
-    def solution_first_star(self, input_value: List[str], input_type: TestEnum) -> int:
+    def solution_first_star(
+        self, input_value: Iterator[str], input_type: TestEnum
+    ) -> int:
         return self.count_id(input_value)
 
-    def solution_second_star(self, input_value: List[str], input_type: TestEnum) -> int:
+    def solution_second_star(
+        self, input_value: Iterator[str], input_type: TestEnum
+    ) -> int:
         return self.count_id_2(input_value)
